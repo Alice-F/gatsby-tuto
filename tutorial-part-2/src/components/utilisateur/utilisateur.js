22afa5db7aa44517584327e0329a0f6a57481c6d
Alice Fabre <alice.fabre@hotmail.fr>
2019-12-02 16:32:54 +0100

    tuto part 4

--- tutorial-part-2/src/components/utilisateur/utilisateur.js
+++ tutorial-part-2/src/components/utilisateur/utilisateur.js
@@ -1,13 +1,26 @@
 import React from "react"
 import styles from "./utilisateur.module.css"
 
-export default props => (
-  <div className={styles.user}>
-    <img src={props.avatar} className={styles.avatar} alt="" />
-    <div className={styles.description}>
-      <h2 className={styles.username}>{props.username}</h2>
-      <p className={styles.excerpt}>{props.excerpt}</p>
-    </div>
-  </div>
-)
+// export default props => (
+//   <div className={styles.user}>
+//     <img src={props.avatar} className={styles.avatar} alt="" />
+//     <div className={styles.description}>
+//       <h2 className={styles.username}>{props.username}</h2>
+//       <p className={styles.excerpt}>{props.excerpt}</p>
+//     </div>
+//   </div>
+// )
 
+const Utilisateur = props => {
+  return (
+    <div className={styles.user}>
+      <img src={props.avatar} className={styles.avatar} alt="" />
+      <div className={styles.description}>
+        <h2 className={styles.username}>{props.username}</h2>
+        <p className={styles.excerpt}>{props.excerpt}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Utilisateur
